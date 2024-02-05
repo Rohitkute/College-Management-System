f6bcccf59640db383988e90294c43b333dbd0a9b
Rohitkute <rohitkute898@gmail.com>
2024-02-06 00:56:00 +0530

    Jay Malhar

---
+++ college-management-system/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./Auth"
+
+function App() {
+  return (
+    // <div className="App">
+      
+    // </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
